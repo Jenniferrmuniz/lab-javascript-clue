547eb96c009b27b19d7910c71d504ef5f1e5198c
Jennifer Muniz <Jen@Jennifers-MacBook-Pro-3.local>
2019-08-21 15:11:55 -0400

    First commit

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -84,11 +84,100 @@ name: Guest House
 name: Patio
 */
 
+
+const mrGreen = {
+    first_name:   'Jacob',
+    last_name:    'Green',
+    color:        'green',
+    description:  'He has a lot of connections',
+    age:          45,
+    image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    occupation:   'Entrepreneur'
+}
+const drOrchid = {
+    first_name:   'Doctor',
+    last_name:    'Orchid',
+    color:        'white',
+    description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    age:          26,
+    image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    occupation:   'Scientist'
+}
+const profPlum = {
+    first_name:   'Victor',
+    last_name:    'Plum',
+    color:        'purple',
+    description:  'Billionaire video game designer',
+    age:          22,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+    occupation:   'Designer'
+}
+const missScarlet = {
+    first_name:   'Kasandra',
+    last_name:    'Scarlet',
+    color:        'red',
+    description:  'She is an A-list movie star with a dark past',
+    age:          31,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+    occupation:   'Actor'
+}
+const mrsPeacock = {
+    first_name:   'Eleanor',
+    last_name:    'Peacock',
+    color:        'blue',
+    description:  'She is from a wealthy family and uses her status and money to earn popularity',
+    age:          36,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    occupation:   'Socialité'
+}
+const mrMustard = {
+    first_name:   'Jack',
+    last_name:    'Mustard',
+    color:        'yellow',
+    description:  'He is a former football player who tries to get by on his former glory',
+    age:          62,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+    occupation:   'Retired Football player'
+}
+const rope = {name: 'rope', weight: 10 }
+const knife = {name: 'knife', weight: 8 }
+const candlestick = {name: 'candlestick', weight: 2 }
+const dumbell = {name: 'dumbell', weight: 30 }
+const poison = {name: 'poison', weight: 2 }
+const axe = {name: 'axe', weight: 15 }
+const bat = {name: 'bat', weight: 13 }
+const trophy = {name: 'trophy', weight: 25 }
+const pistol = {name: 'pistol', weight: 20 }
+
 // Characters Collection
-const charactersArray = [];
+const charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = ['Dining Room', 'Conservator', 'Kitchen', 'Study', 'Library', 'Billard Room', 'Lounge', 'Ballroom', 'Hall', 'Spa', 'Living Room', 'Observatory', 'Theater', 'Guest House', 'Patio'];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [rope, knife, candlestick, dumbell, poison, axe, bat, trophy, pistol];
+
+let mysteryEnvelope = {};
+
+function randomSelector(arr){
+    var rand = arr[Math.floor(Math.random() * arr.length)];
+    return rand;
+}
+
+function pickMystery(){
+    mysteryEnvelope.suspect = randomSelector(charactersArray);
+    mysteryEnvelope.room = randomSelector(roomsArray);
+    mysteryEnvelope.weapon = randomSelector(weaponsArray);
+}
+
+function revealMystery(mysteryEnvelope){
+    let str = mysteryEnvelope.suspect.first_name + ' ' + mysteryEnvelope.suspect.last_name + ' killed Mr. Boddy using the ' + mysteryEnvelope.weapon.name + ' in the ' + mysteryEnvelope.room +'!!!!';
+    return str;
+}
+
+
+
+
+pickMystery();
+console.log(revealMystery(mysteryEnvelope));
